4d02a621c5546a97d6abdae587378cea548b131a
bojanwik <bojan.wik@o2.pl>
2017-01-07 16:43:57 +0100

    Update wisielec.py
    Poprawki

--- 16.12.05/wisielec.py
+++ 16.12.05/wisielec.py
@@ -1,4 +1,5 @@
 # Uproszczona wersja gry 'Wisielec'
+#! /usr/bin/python3
 
 from random import choice
 
@@ -7,24 +8,48 @@ theWord = choice(words)
 exitCommand = 'EXIT'
 hiddenWord = ''
 count = 0
+guessedLetters = []
+
+def oneLetterGuess(letter):
+    global guessedLetters, theWord, count, hiddenWord
+    if letter not in guessedLetters:
+        if letter in theWord:
+            count += 1
+            for i in range(len(theWord)):
+                if theWord[i] == letter:
+                    hiddenWord = hiddenWord[:i] + letter + hiddenWord[i +1:]
+        else:
+            count += 1
+        guessedLetters += letter
+    else:
+        print('Juz probowales ta litere.')
 
 for letter in theWord:
     hiddenWord += letter.replace(letter, '*')
 
 while hiddenWord != theWord:
+    print('\n')
+    print('-' * 20)
+    print('Wykorzystane tury: {}'.format(count))
+    print('Wykorzystane litery: {}'.format(guessedLetters))
     print('\nHaslo: {}'.format(hiddenWord))
-    guess = input('Odgadnij litere: ').upper()
-    if guess in theWord:
-        for i in range(len(theWord)):
-            if theWord[i] == guess:
-                hiddenWord = hiddenWord[:i] + guess + hiddenWord[i +1:]
-        count += 1
-    elif guess == exitCommand:
-        hiddenWord = theWord
+    guess = input('Odgadnij litere/slowo: ').upper()
+    if guess.isalpha():
+        if guess == exitCommand:
+            hiddenWord = theWord
+        elif len(guess) == 1:
+            oneLetterGuess(guess)
+        elif guess == theWord:
+            count += 1
+            hiddenWord = theWord
     else:
-        count += 1
+        print('Musisz podac litere.')
 
 if hiddenWord == theWord and guess != exitCommand:
-    print('\nZgadles! Haslo to: {}\nLiczba wykorzystanych tur: {}\n'.format(hiddenWord, count))
+    print('\n')
+    print('-' * 20)
+    print('Zgadles! Haslo to: {}\nLiczba wykorzystanych tur: {}\n'.format(hiddenWord, count))
 else:
-    print('\nPrzegrales. Haslo to: {}\nLiczba wykorzystanych tur: {}\n'.format(hiddenWord, count))
+    print('\n')
+    print('-' * 20)
+    print('Przegrales. Haslo to: {}\nLiczba wykorzystanych tur: {}\n'.format(hiddenWord, count))
