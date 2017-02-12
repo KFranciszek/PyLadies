ab0e294c1e9ab4d0de8f5db581fb3a481b1e6569
bojanwik <bojan.wik@o2.pl>
2017-02-12 13:11:35 +0100

    Update sw_all_heroes.py

--- 11_2017.01.30/sw_all_heroes.py
+++ 11_2017.01.30/sw_all_heroes.py
@@ -3,9 +3,10 @@
 
 import json
 
-py_10_2File = open('py_10_2', 'r')
-py_10_2Content = py_10_2File.read()
-swList = json.loads(py_10_2Content)
+with open('py_10_2', 'r') as py_10_2File:
+    py_10_2Content = py_10_2File.read()
+    swList = json.loads(py_10_2Content)
+
 swListFiltered = list(filter(lambda a: a['name'] != 'unknown' and a['mass'] != 'unknown' and a['gender'] != 'unknown' and a['gender'] != 'n/a', swList))
 
 with open('sw_all_heroes', 'w') as file:
@@ -16,5 +17,3 @@ with open('sw_all_heroes', 'w') as file:
 			file.write('{} wazy {} kg i jest kobieta.\n'.format(character['name'], character['mass']))
 		else:
 			file.write('{} wazy {} kg i jest hermafrodyta.\n'.format(character['name'], character['mass']))
-
-py_10_2File.close()
